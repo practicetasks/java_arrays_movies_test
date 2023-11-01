4c94957f27a283ad95b4d15a612ae29fdc885321
= <=>
2023-11-01 10:03:26 +0600

    .

--- src/Manager.java
+++ src/Manager.java
@@ -1,46 +1,46 @@
 public class Manager {
     // TODO: Находит фильм с самым длинным названием среди массива фильмов.
     public Movie findMovieWithLongestTitle(Movie[] movies){
-
+        return null;
     }
 
     // TODO: Находит фильм с наибольшим количеством жанров среди массива фильмов.
     public Movie findMovieWithMostGenres(Movie[] movies) {
-
+        return null;
     }
 
     // TODO: Возвращает количество фильмов, выпущенных в заданном диапазоне лет.
     public int getAmountOfMoviesInYearRange(Movie[] movies, int startYear, int endYear) {
-
+        return 0;
     }
 
 
     // TODO: Вычисляет среднюю продолжительность фильмов, выпущенных в заданном диапазоне лет.
     public double calculateAverageDurationInYearRange(Movie[] movies, int startYear, int endYear) {
-
+        return 0.0;
     }
 
 
 
     // TODO: Находит фильмы с самым высоким и самым низким рейтингом среди массива фильмов.
     public double[] findHighestAndLowestRatings(Movie[] movies) {
-
+        return null;
     }
 
     // TODO: Находит все фильмы с заданным рейтингом среди массива фильмов.
     public Movie[] findMoviesByRating(Movie[] movies, double rating) {
-
+        return null;
     }
 
 
     // TODO: Определяет самый популярный жанр среди массива фильмов.
     public String findMostPopularGenre(Movie[] movies) {
-
+        return null;
     }
 
 
     // TODO: Возвращает массив всех жанров среди массива фильмов.
     public String[] getAllGenres(Movie[] movies) {
-
+        return null;
     }
 }
